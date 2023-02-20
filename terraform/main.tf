2622134a240b2d4ba40ecafc055b0cbe671df48b
araltiparmak <5054445+araltiparmak@users.noreply.github.com>
2023-02-20 20:28:41 +0100

    create terraform (#3)
    add terraform config

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "vite_react_project" {
+  bucket = "vite-react-project"
+}
+
+resource "aws_s3_bucket_versioning" "vite_react_project_versioning" {
+  bucket = aws_s3_bucket.vite_react_project.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_object" "deploy" {
+  key    = "deploy"
+  bucket = aws_s3_bucket.vite_react_project.id
+  acl    = "private"
+}
